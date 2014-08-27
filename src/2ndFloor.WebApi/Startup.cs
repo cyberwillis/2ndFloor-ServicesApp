6e4e67bdfaf548fb779a74c9dbfe06f7d13b9220
Willismar <willismar@uol.com.br>
2014-08-27 13:10:05 -0300

    Estrutura inicial

---
+++ src/2ndFloor.WebApi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_2ndFloor.WebApi.Startup))]
+
+namespace _2ndFloor.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
