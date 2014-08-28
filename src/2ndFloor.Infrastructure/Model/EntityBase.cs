74f57f55a43b67519ad1a704bc386774bae5a083
Willismar <willismar@uol.com.br>
2014-08-28 19:21:39 -0300

    Estrutura para o Model e RepositoryEF

---
+++ src/2ndFloor.Infrastructure/Model/EntityBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace _2ndFloor.Infrastructure
+{
+    public class EntityBase<TId> : IEntityBase<TId>
+    {
+        public TId Id { get; set; }
+
+        protected IList<BusinessRule> brokenRules = new List<BusinessRule>();
+
+        //protected abstract void Validate();
+
+        public virtual IList<BusinessRule> GetBrokenRules()
+        {
+            return brokenRules;
+        }
+
+        public virtual void AddBrokenRule(BusinessRule businessRule)
+        {
+            brokenRules.Add(businessRule);
+        }
+
+        public virtual StringBuilder GetErrorMessages()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Erros encontrados:");
+            foreach (var error in brokenRules)
+            {
+                sb.AppendLine(error.Rule + "<br/>");
+            }
+            return sb;
+        }
+    }
+}
---
+++ src/2ndFloor.Infrastructure/Model/IAggregateRoot.cs
@@ -0,0 +1,6 @@
+﻿namespace _2ndFloor.Infrastructure
+{
+    public interface IAggregateRoot
+    {
+    }
+}
