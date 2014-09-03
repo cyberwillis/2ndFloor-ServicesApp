2e5dc67dd604ba33bd8a3825e3d898d86881de33
rsadev <rafael.sanjos@gmail.com>
2014-09-03 00:18:56 -0300

    Conversão de lista no AnuncioExtension

--- src/SecondFloor.Service/ExtensionMethods/AnuncioExtensionMethod.cs
+++ src/SecondFloor.Service/ExtensionMethods/AnuncioExtensionMethod.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using SecondFloor.DataContracts.DTO;
 using SecondFloor.Model;
@@ -10,9 +11,9 @@ namespace SecondFloor.Service.ExtensionMethods
         public static Anuncio ConvertToAnuncio(this AnuncioDto anuncioDto)
         {
             var anuncio = new Anuncio();
-            
-            if(anuncioDto.Ofertas.Any())
-                anuncio.Ofertas = anuncioDto.Ofertas.ConvertToListaDeOfertas();
+
+            if (anuncioDto.Ofertas.Any())
+                anuncio.Ofertas = anuncioDto.Ofertas.ConvertToListaDeOfertas().ToList();
 
             if (anuncioDto.AnoInicio > 0 && anuncioDto.MesInicio > 0 && anuncioDto.DiaInicio > 0)
                 anuncio.DataInicio = new DateTime(anuncioDto.AnoInicio, anuncioDto.MesInicio, anuncioDto.DiaInicio);
