fb317df5ba7d37cfe7be52022a998f5d93818f8e
Willismar <willismar@uol.com.br>
2014-09-08 18:58:44 -0300

    Fix de Id para AnuncianteDto

--- src/SecondFloor.DataContracts/DTO/AnuncianteDto.cs
+++ src/SecondFloor.DataContracts/DTO/AnuncianteDto.cs
@@ -4,10 +4,12 @@ namespace SecondFloor.DataContracts.DTO
 {
     public class AnuncianteDto
     {
-        public string RazaoSocial;
-        public string Cnpj;
-        public string Email;
-        public string Token;
-        public IList<AnuncioDto> Anuncios;
+        public string Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public IList<AnuncioDto> Anuncios { get; set; }
+        
     }
 }
