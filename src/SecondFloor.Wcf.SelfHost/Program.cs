5c59f79c36795c3041f23d7c03dd72db3f96a7ce
Willismar <willismar@uol.com.br>
2014-09-09 01:12:41 -0300

    Implementacao preliminar de WCF SelfHost

--- src/SecondFloor.Wcf.SelfHost/Program.cs
+++ src/SecondFloor.Wcf.SelfHost/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using SecondFloor.Model;
+using SecondFloor.Service;
+using SecondFloor.ServiceContracts;
+using StructureMap;
 
 namespace SecondFloor.Wcf.SelfHost
 {
@@ -10,7 +12,50 @@ namespace SecondFloor.Wcf.SelfHost
     {
         static void Main(string[] args)
         {
+            //IoC Container
 
+            var host = new ServiceHost(typeof(AnuncioService));
+            try
+            {
+                host.Open();
+
+                Console.WriteLine("Service is up and running with these endpoints:");
+
+                ServiceEndpointCollection endpoints = host.Description.Endpoints;
+
+                foreach (ServiceEndpoint serviceEndpoint in endpoints)
+                {
+                    Console.WriteLine(serviceEndpoint.Address);
+                }
+
+                Console.WriteLine("Type <ENTER> to close");
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (FaultException fe)
+            {
+                Console.WriteLine(fe.Reason);
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine(ce.Message);
+            }
+            catch (TimeoutException te)
+            {
+                Console.WriteLine(te.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                /*if (host.State == CommunicationState.Faulted)
+                    host.Open();*/
+
+                
+            }
         }
     }
 }
