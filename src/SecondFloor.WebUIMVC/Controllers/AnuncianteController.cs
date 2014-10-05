b8b4148ae950389f32b963c67f6723388d4bd13f
Willismar <willismar@uol.com.br>
2014-10-04 22:57:17 -0300

    Inicio Implementacao MVC

---
+++ src/SecondFloor.WebUIMVC/Controllers/AnuncianteController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SecondFloor.WebUIMVC.Models;
+
+namespace SecondFloor.WebUIMVC.Controllers
+{
+    public class AnuncianteController : Controller
+    {
+        // GET: Anunciante
+        public ActionResult Index()
+        {
+            var anunciante = new AnuncianteViewModels();
+
+            //anunciante.Enderecos = new List<EnderecoViewModels>();
+
+            return View(anunciante);
+        }
+
+        [HttpPost]
+        public ActionResult Index ([Bind(Exclude = "Id")] AnuncianteViewModels anunciante)
+        {
+            if (! ModelState.IsValid )
+            {
+                //anunciante.Enderecos = new LinkedList<EnderecoViewModels>();
+
+                return View(anunciante);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
