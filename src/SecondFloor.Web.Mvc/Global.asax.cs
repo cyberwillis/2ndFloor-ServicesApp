0b411c22ac1e0d8d2024b1f308275d199052e588
Willismar <willismar@uol.com.br>
2014-10-09 13:38:35 -0300

    incluido script correto para criacao ou atualização de banco de dados com AutomaticMigrationDataLossAllowed e AutomaticMigrationsEnabled ativados

--- src/SecondFloor.Web.Mvc/Global.asax.cs
+++ src/SecondFloor.Web.Mvc/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using SecondFloor.RepositoryEF;
+using SecondFloor.RepositoryEF.Migrations;
 
 namespace SecondFloor.Web.Mvc
 {
@@ -14,6 +15,9 @@ namespace SecondFloor.Web.Mvc
     {
         protected void Application_Start()
         {
+            //Inicializacao do Banco de dados e migração caso mudança de modelo
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AnuncianteContext, AnuncianteModelConfiguration>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
